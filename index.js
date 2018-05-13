7702811f392763219bfdb8553491ecdb5af22f47
laurenelizabethmail@yahoo.com <laurenelizabethmail@yahoo.com>
2018-05-13 18:28:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHelloToGrandma(string) {
+  var lowercase = "hello!"
+  var uppercase = "HELLO!"
 if (string === string.toLowerCase())
   return "I can't hear you!.";
-else if (string === string.tpUpperCase())
+else if (string === string.toUpperCase())
   return "YES INDEED!";
 else if (string === "I love you, Grandma.")
   return "I love you, too."
