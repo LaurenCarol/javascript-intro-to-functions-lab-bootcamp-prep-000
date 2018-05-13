c06ec36eca7eb053afd17b8914bcf53eff7febc0
laurenelizabethmail@yahoo.com <laurenelizabethmail@yahoo.com>
2018-05-13 18:29:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
-function sayHelloToGrandma(string) {
+function sayHiToGrandma(string) {
   var lowercase = "hello!"
   var uppercase = "HELLO!"
 if (string === string.toLowerCase())
